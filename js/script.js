44b28f9acc970ddb58d318293aebc3abca0e19be
holyshitplayer <nemirdota@gmail.com>
2022-04-14 19:15:14 +0300

    Адаптив главной страницы

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let menuLinks = document.getElementsByClassName("header__links__link")
+let menuLinks = document.getElementsByClassName("header__menu__link")
 for (let i = 0; i < menuLinks.length; i++) {
     let menuLink = (menuLinks[i].href).replace("#", "")
     let url = (window.location.href).replace("#", "")
@@ -6,3 +6,30 @@ for (let i = 0; i < menuLinks.length; i++) {
         menuLinks[i].classList.add("active");
     }
 };
+
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        const menu = new MmenuLight(
+            document.querySelector(".header__menu"),
+            "(max-width: 992px)"
+        );
+
+        const navigator = menu.navigation({
+            theme: "light",
+            title: "Меню",
+        });
+        const drawer = menu.offcanvas();
+
+        document.querySelector('.header__menu__open-menu')
+            .addEventListener('click', (evnt) => {
+                evnt.preventDefault();
+                drawer.open();
+            });
+
+        document.querySelector('.header__menu__close-menu')
+        .addEventListener('click', (evnt) => {
+            evnt.preventDefault();
+            drawer.close();
+        });
+    }
+);
